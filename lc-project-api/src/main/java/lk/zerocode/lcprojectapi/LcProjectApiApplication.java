3f49c24658e1cde930954c2435bdf281166e869e
AnjanaTharanga24 <anjana24myprof@gmail.com>
2024-03-20 04:46:55 +0000

    initial commit

---
+++ lc-project-api/src/main/java/lk/zerocode/lcprojectapi/LcProjectApiApplication.java
@@ -0,0 +1,13 @@
+package lk.zerocode.lcprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcProjectApiApplication.class, args);
+	}
+
+}
---
+++ lc-project-api/src/test/java/lk/zerocode/lcprojectapi/LcProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.zerocode.lcprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
